46d85a5c7938d71313b6f847ded28416c78f384f
Kamila Nazirova <kamilanazirova02@gmail.com>
2024-04-17 18:05:49 +0300

    transport.tsx03

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+declare module '*.svg' {
+    let path: string;
+    export default path;
+}
+declare module '*.png' {
+    let path: string;
+    export default path;
+}
--- src/app.tsx
+++ src/app.tsx
@@ -6,7 +6,6 @@ import Transport  from './pages/transport'
 const App = () => {
   return (
     <>
-    <h1> Почему страница не отображается </h1>
     <Transport/>
     </>
   );
