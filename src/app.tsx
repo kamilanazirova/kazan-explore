f5df08ef94904d85b59d67732cd197d32d72fcd2
Liliya Vasilova <vasilovaliliya123@gmail.com>
2024-05-02 20:41:31 +0300

    (#5) Education page on components

--- src/app.tsx
+++ src/app.tsx
@@ -3,17 +3,19 @@ import React from 'react';
 import First from './pages/first'
 import Transport  from './pages/transport'
 import Sport from './pages/sport'
+import Education from './pages/education'
 
-import './style/first.css'
+//import './style/first.css'
 //import './style/transport.css'
 //import './style/sport.css'
 import './style/main.css'
+import'./style/education.css'
 
 
 const App = () => {
   return (
     <>
-    <First/>
+    <Education/>
     </>
   );
 };
---
+++ src/components/card-education/card-education.styled.ts
@@ -0,0 +1,35 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+    top: 30px;
+    width: 280px;
+    background-color: #D9D9D9;
+    border-radius: 30px;
+    margin: 10px;
+`;
+
+export const Photo = styled.img`
+    width: 260px;
+    border-radius: 20px;
+    padding: 10px;
+    top: 400px;
+`
+export const Text = styled.p`
+    color: #000000;
+    font-size: 13px;
+    padding: 7px 3px 3px 3px;
+    margin: 0 20px;
+    line-height: 1.3;
+`;
+
+export const Head = styled.p`
+    background-color: #ACA9A9;
+    border-radius: 30px 30px 3px 3px;
+    padding: 13px 23px 7px 23px;
+    height: 40px;
+    font-size: 18px;
+    color: #000000;
+    margin: 0;
+    font-weight: 700;
+    line-height: 1.2;
+`;
