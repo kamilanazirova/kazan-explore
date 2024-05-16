c5c4999b5438f46f3a17e4895b31b878e72c393f
Kamila Nazirova <kamilanazirova02@gmail.com>
2024-05-16 13:51:56 +0300

    Поменяли error-boundary.tsx

--- src/components/error-boundary.tsx
+++ src/components/error-boundary.tsx
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react'
 
-class ErrorBoundary extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false };
-  } 
+export class ErrorBoundary extends React.Component {
+  state = {
+    error: false
+  }
 
-  componentDidCatch(error, errorInfo) {
-    console.error(error, errorInfo);
-    this.setState({ hasError: true });
+  static getDerivedStateFromError() {
+    return {
+      error: true
+    }
   }
 
   render() {
-    if (this.state.hasError) {
-      return <h1>Something went wrong...</h1>;
+    if (this.state.error) {
+      return <h1>Something went wrong.</h1>
     }
 
-    return this.props.children;
+    return this.props.children
   }
 }
-
-export default ErrorBoundary;
