acf6520d3b26b22181fae62dfd604671c331d30a
Kamila Nazirova <kamilanazirova02@gmail.com>
2024-05-25 03:51:51 +0300

    Clear css styles

--- src/app.tsx
+++ src/app.tsx
@@ -5,11 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { globalStyles } from './global-styles';
 import { PageRoutes } from './routes';
 
-import './style/first.css'
-import './style/transport.css'
-import './style/sport.css'
-import './style/education.css'
-
 const App = () => {
   return (
     <BrowserRouter>
