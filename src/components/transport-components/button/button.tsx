02a8f382671d056d5d2b240d82ba0c062f96ff96
Kamila Nazirova <kamilanazirova02@gmail.com>
2024-05-29 14:51:21 +0300

    Replace map to botton copmonent

--- src/components/transport-components/button/button.tsx
+++ src/components/transport-components/button/button.tsx
@@ -3,20 +3,24 @@ import React from "react";
 import './button.styled';
 import { BottonWrapper, StyledButton, TransportType } from "./button.styled";
 import { Link } from "react-router-dom";
+import { URLs } from "../../../__data__/urls";
+import { Map } from "../../transport-components/map";
+import map from '../../../assets/transport/trips/1.png'
 
 
 export function Button({ numbers, type }) {
     return (
         <BottonWrapper>
-            <TransportType>{ type }</TransportType>
+            <TransportType>{type}</TransportType>
             {numbers.map((number, index) => (
                 <StyledButton key={index}>
-                    <Link to="" >
+                    <Link to={URLs.tripNumber.getUrl(number)} >
                         {`№${number}`}
                     </Link>
                 </StyledButton>
             ))
             }
+            <Map index="" image={map} />
         </BottonWrapper >
     );
 }
