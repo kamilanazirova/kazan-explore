7a2dee1e025258e4149f18aa340837df3df81534
Kamila Nazirova <kamilanazirova02@gmail.com>
2024-06-02 13:31:20 +0300

    Navigation controls changed

--- src/components/header-links/header-links.styled.ts
+++ src/components/header-links/header-links.styled.ts
@@ -77,54 +77,6 @@ export const FavouritesLink = styled(Link) <{
   }}
 `;
 
-export const FavouritesTitle = styled.span`
-  @media(max-width: 680px) {
-    display: none;
-  }
-
-  @media(max-width: 540px) {
-    display: block;
-  }
-`
-
-export const GradientLink = styled(Link)`
-  border-radius: 12px;
-  padding: 12px 20px;
-  cursor: pointer;
-  color: var(--dark-text-color);
-  background: rgb(23,233,107);
-  background: -moz-linear-gradient(110deg, rgba(23,233,107,1) 0%, rgba(36,215,194,1) 100%);
-  background: -webkit-linear-gradient(110deg, rgba(23,233,107,1) 0%, rgba(36,215,194,1) 100%);
-  background: linear-gradient(110deg, rgba(23,233,107,1) 0%, rgba(36,215,194,1) 100%);
-  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#17e96b",endColorstr="#24d7c2",GradientType=1);
-`;
-
-export const BorderLink = styled(Link)`
-  border-radius: 12px;
-  padding: 12px 20px;
-  cursor: pointer;
-  color: #20DCAA;
-  border: 1px solid #20DCAA;
-`;
-
-
-
-
-
-
-
-
-export const HeaderBlock = styled.header`
-    width: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    padding: 0;
-    margin: 0;
-    color: #FFFFFF;
-    font-size: 15px;
-    font-weight: 600;
-    line-height: 10px;
-`;
-
 export const StyledMenu = styled.ul`
     display: flex;
     margin: 0 auto
--- src/components/header-links/header-links.tsx
+++ src/components/header-links/header-links.tsx
@@ -6,17 +6,17 @@ import logo from '../../assets/logo.svg'
 import { StyledNav } from './header-links.styled';
 
 import { URLs } from "../../__data__/urls";
-import { LoginContext  } from '../../context/login-context';
+import { LoginContext } from '../../context/login-context';
 import { Link } from 'react-router-dom';
 import { ImgLogo, MenuLi, OverlayLogin, StyledLogin, StyledMenu } from './header-links.styled';
 
 const nav = {
-    places: { title: "Интересные места", href: URLs.ui.places },
-    transport: { title: "Транспорт и инфраструктура", href: URLs.ui.transport },
-    sport: { title: "Спорт и развлечения", href: URLs.ui.sport },
-    history: { title: "История и культура", href: URLs.ui.history },
-    education: { title: "Наука и образование", href: URLs.ui.education },
-  }
+  places: { title: "Интересные места", href: URLs.ui.places },
+  transport: { title: "Транспорт и инфраструктура", href: URLs.ui.transport },
+  sport: { title: "Спорт и развлечения", href: URLs.ui.sport },
+  history: { title: "История и культура", href: URLs.ui.history },
+  education: { title: "Наука и образование", href: URLs.ui.education },
+}
 
 export function HeaderLinks({ isOpen }) {
   const { currentUser, setCurrentUser } = useContext(LoginContext);
@@ -29,49 +29,51 @@ export function HeaderLinks({ isOpen }) {
 
   return (
     <StyledNav isOpen={isOpen}>
-        <Link to={URLs.baseUrl}>
-          <ImgLogo src={logo} alt="логотип сайта" />
-        </Link>
-        <StyledMenu>
-          {URLs.ui.places && (<MenuLi>
-            <Link
-              to={nav.places.href}>{nav.places.title}
-            </Link>
-          </MenuLi>
-          )}
-          {URLs.ui.transport && (<MenuLi>
-            <Link
-              to={nav.transport.href}>{nav.transport.title}
-            </Link>
-          </MenuLi>
-          )}   
-          {URLs.ui.sport && (<MenuLi>
-            <Link
-              to={nav.sport.href}>{nav.sport.title}
-            </Link>
-          </MenuLi>
-          )}
-          {URLs.ui.history && (<MenuLi>
-            <Link
-              to={nav.history.href}>{nav.history.title}
-            </Link>
-          </MenuLi>
-          )}
-          {URLs.ui.education && (<MenuLi>
-            <Link
-              to={nav.education.href}>{nav.education.title}
-            </Link>
-          </MenuLi>
-          )}
-        </StyledMenu>
-        
-        <OverlayLogin>
-          <StyledLogin>
-            {!isAuth&&<Link to={URLs.ui.entrance}>Войти</Link>}
-            {isAuth&&<Link onClick={onLogOut} to={URLs.ui.entrance}>Выйти</Link>}
-          </StyledLogin>
-        </OverlayLogin>
+      <Link to={URLs.baseUrl}>
+        <ImgLogo src={logo} alt="логотип сайта" />
+      </Link>
+      <StyledMenu>
+        {URLs.ui.places && (<MenuLi>
+          <Link
+            to={nav.places.href}>{nav.places.title}
+          </Link>
+        </MenuLi>
+        )}
+        {URLs.ui.transport && (<MenuLi>
+          <Link
+            to={nav.transport.href}>{nav.transport.title}
+          </Link>
+        </MenuLi>
+        )}
+        {URLs.ui.sport && (<MenuLi>
+          <Link
+            to={nav.sport.href}>{nav.sport.title}
+          </Link>
+        </MenuLi>
+        )}
+        {URLs.ui.history && (<MenuLi>
+          <Link
+            to={nav.history.href}>{nav.history.title}
+          </Link>
+        </MenuLi>
+        )}
+        {URLs.ui.education && (<MenuLi>
+          <Link
+            to={nav.education.href}>{nav.education.title}
+          </Link>
+        </MenuLi>
+        )}
+      </StyledMenu>
 
-  </StyledNav>
+      <OverlayLogin>
+        {URLs.ui.entrance && (
+          <StyledLogin>
+            {!isAuth && <Link to={URLs.ui.entrance}>Войти</Link>}
+            {isAuth && <Link onClick={onLogOut} to={URLs.ui.entrance}>Выйти</Link>}
+          </StyledLogin>
+        )}
+      </OverlayLogin>
+
+    </StyledNav>
   );
 }
