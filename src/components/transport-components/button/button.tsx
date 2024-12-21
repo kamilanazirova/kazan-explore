28a0f1eb2c6c84d1a6fc6f8ee5d0b60d5245db4c
Kamila Nazirova <kamilanazirova02@gmail.com>
2024-12-21 08:46:02 +0300

    rtq for transport page

--- src/components/transport-components/button/button.tsx
+++ src/components/transport-components/button/button.tsx
@@ -2,25 +2,18 @@ import React from "react";
 
 import './button.styled';
 import { BottonWrapper, StyledButton, TransportType } from "./button.styled";
-import { Link } from "react-router-dom";
-import { URLs } from "../../../__data__/urls";
-/* import { Map } from "../../transport-components/map";
-import map from '../../../assets/transport/trips/1.png' */
 
 
 export function Button({ numbers, type, onBusClick }) {
     return (
         <BottonWrapper>
             <TransportType>{type}</TransportType>
-            {numbers?.map((number, index) => (
+            {numbers.map((number, index) => (
                 <StyledButton  onClick={() => onBusClick(number)} key={index}>
-                    {/* <Link to={URLs.ui.tripNumber.getUrl(number)} > */}
                         {`№${number}`}
-                    {/* </Link> */}
                 </StyledButton>
             ))
             }
-            {/* <Map index="" image={map} /> */}
         </BottonWrapper >
     );
 }
